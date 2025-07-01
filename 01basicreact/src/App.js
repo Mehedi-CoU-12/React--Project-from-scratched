724980e9f020611bef18be39c6925f1c2b503a01
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-07-01 18:55:05 +0600

    basic  structure of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div style={{backgroundColor:'darkgreen',height:'100vh',margin:'0px'}} className="App">
+      <h1>Mehedi</h1>
+    </div>
+  );
+}
+
+export default App;
