f3e841f3b7d69c1de56e401f346a4cd696984f25
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-07-03 11:40:14 +0600

    make the code modular

--- 05bgchanger/src/App.jsx
+++ 05bgchanger/src/App.jsx
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
 
+const colors = ["Red", "Green", "Blue", "Maroon", "Yellow", "Azure", "Black"];
+const textColor = [
+    "white",
+    "white",
+    "white",
+    "white",
+    "black",
+    "black",
+    "white",
+];
+
 function App() {
     const [color, setColor] = useState("olive");
 
@@ -11,55 +22,16 @@ function App() {
         <div className="w-full h-screen" style={{ backgroundColor: color }}>
             <div className="flex fixed justify-center bottom-12 inset-x-2 px-2">
                 <div className="flex flex-wrap justify-center gap-3 bg-white px-2 py-1 rounded-3xl">
-                    <button
-                        onClick={() => changeBgColor("red")}
-                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
-                        style={{ backgroundColor: "red" }}
-                    >
-                        Red
-                    </button>
-                    <button
-                        onClick={() => changeBgColor("green")}
-                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
-                        style={{ backgroundColor: "green" }}
-                    >
-                        green
-                    </button>
-                    <button
-                        onClick={() => changeBgColor("blue")}
-                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
-                        style={{ backgroundColor: "blue" }}
-                    >
-                        blue
-                    </button>
-                    <button
-                        onClick={() => changeBgColor("maroon")}
-                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
-                        style={{ backgroundColor: "maroon" }}
-                    >
-                        maroon
-                    </button>
-                    <button
-                        onClick={() => changeBgColor("yellow")}
-                        className="outline-none px-2 py-1 rounded-full text-black shadow-lg"
-                        style={{ backgroundColor: "yellow" }}
-                    >
-                        yellow
-                    </button>
-                    <button
-                        onClick={() => changeBgColor("azure")}
-                        className="outline-none px-2 py-1 rounded-full text-black shadow-lg"
-                        style={{ backgroundColor: "azure" }}
-                    >
-                        azure
-                    </button>
-                    <button
-                        onClick={() => changeBgColor("black")}
-                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
-                        style={{ backgroundColor: "black" }}
-                    >
-                        Dark
-                    </button>
+                    {colors.map((colour, index) => (
+                        <button
+                            key={index}
+                            onClick={() => changeBgColor(colour)}
+                            className={`"outline-none px-2 py-1 rounded-full text-${textColor[index]} shadow-lg"`}
+                            style={{ backgroundColor: colour }}
+                        >
+                            {colour}
+                        </button>
+                    ))}
                 </div>
             </div>
         </div>
