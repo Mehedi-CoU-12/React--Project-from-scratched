c7fd67143c00c3a266421e70bce803d24de41cb9
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-07-03 11:31:24 +0600

    improve the bg color project ui

--- 05bgchanger/src/App.jsx
+++ 05bgchanger/src/App.jsx
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-    const colors = ["red", "green", "blue", "white", "black", "yellow"];
+    const [color, setColor] = useState("olive");
 
-    function changeBgColor(index) {
-        const div = document.getElementById("bg");
-        div.style.backgroundColor = colors[index];
+    function changeBgColor(colour) {
+        setColor(colour);
     }
 
     return (
-        <div style={{ height: "1000px" }} id="bg">
-            <div>
-                <h1>sumon bro</h1>
-            </div>
-            <div>
-                {colors.map((color, index) => (
-                    <button key={index} onClick={() => changeBgColor(index)}>
-                        {color}
+        <div className="w-full h-screen" style={{ backgroundColor: color }}>
+            <div className="flex fixed justify-center bottom-12 inset-x-2 px-2">
+                <div className="flex flex-wrap justify-center gap-3 bg-white px-2 py-1 rounded-3xl">
+                    <button
+                        onClick={() => changeBgColor("red")}
+                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
+                        style={{ backgroundColor: "red" }}
+                    >
+                        Red
                     </button>
-                ))}
+                    <button
+                        onClick={() => changeBgColor("green")}
+                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
+                        style={{ backgroundColor: "green" }}
+                    >
+                        green
+                    </button>
+                    <button
+                        onClick={() => changeBgColor("blue")}
+                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
+                        style={{ backgroundColor: "blue" }}
+                    >
+                        blue
+                    </button>
+                    <button
+                        onClick={() => changeBgColor("maroon")}
+                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
+                        style={{ backgroundColor: "maroon" }}
+                    >
+                        maroon
+                    </button>
+                    <button
+                        onClick={() => changeBgColor("yellow")}
+                        className="outline-none px-2 py-1 rounded-full text-black shadow-lg"
+                        style={{ backgroundColor: "yellow" }}
+                    >
+                        yellow
+                    </button>
+                    <button
+                        onClick={() => changeBgColor("azure")}
+                        className="outline-none px-2 py-1 rounded-full text-black shadow-lg"
+                        style={{ backgroundColor: "azure" }}
+                    >
+                        azure
+                    </button>
+                    <button
+                        onClick={() => changeBgColor("black")}
+                        className="outline-none px-2 py-1 rounded-full text-white shadow-lg"
+                        style={{ backgroundColor: "black" }}
+                    >
+                        Dark
+                    </button>
+                </div>
             </div>
         </div>
     );
