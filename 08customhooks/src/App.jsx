97bd8576f17e7f6a3f836b87aa3badc9eb199e1d
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-07-06 17:43:35 +0600

    custorm react hook is created for converting currency

--- 08customhooks/src/App.jsx
+++ 08customhooks/src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InputBox from "./components/InputBox";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
+
+    const data=useCurrencyInfo('usd');
+    console.log(data);
+
+
     return (
         <>
             <div className="bg-slate-500 rounded-md flex flex-col items-center justify-center h-52 w-66 m-28 " >
-                <InputBox name='From' />
+                {/* <InputBox name='From' />
                 <button className="text-white text-xl bg-black w-full m-3 rounded-2xl " > swap</button>
-                <InputBox name='To' />
+                <InputBox name='To' /> */}
             </div>
         </>
     );
---
+++ 08customhooks/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useCurrencyInfo(currency = "bdt") {
+    const [data, setData] = useState({});
+
+    //find today date
+    const dateString = new Date();
+    const date = new Date(dateString);
+    const formattedDate = date.toISOString().split("T")[0];
+
+    useEffect(() => {
+        const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${formattedDate}/v1/currencies/${currency}.json`;
+        const response = axios.get(url);
+
+        response
+            ?.then((res) => res?.data)
+            .then((value) => {
+                setData(value);
+            });
+    }, [currency]);
+
+    return data;
+}
+
+export default useCurrencyInfo;
