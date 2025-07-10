6f1b8ea9956b7548ff3ada14966ebdd845f03321
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-07-10 12:02:25 +0600

    apply lazy loading in react js

--- 15codespitting/src/App.jsx
+++ 15codespitting/src/App.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Store from "./components/Store";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Store from "./components/Store";
+import Loader from "./components/Layout/Loader";
+
+const Home = lazy(() => import("./components/Home"));
+const About = lazy(() => import("./components/About"));
+const Store = lazy(() => import("./components/Store"));
+// const Home = lazy(() => wait(1000).then(() => import("./components/Home")));
+// const About = lazy(() => wait(1000).then(() => import("./components/About")));
+// const Store = lazy(() => wait(1000).then(() => import("./components/Store")));
 
 function App() {
     return (
         <div>
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/store" element={<Store />} />
-            </Routes>
+            <Suspense fallback={<Loader />}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/store" element={<Store />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 }
 
 export default App;
+
+function wait(time) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, time);
+    });
+}
--- 15codespitting/src/components/Home.jsx
+++ 15codespitting/src/components/Home.jsx
@@ -1,21 +1,30 @@
-import React from "react";
-import {sum} from '../sum'
+import React, { useState } from "react";
+import { sum } from "../sum";
+import AdminData from "./AdminData";
 
 function Home() {
+    const [isAdmin, setIsAdmin] = useState(false);
 
-    const handleClick=()=>{
-        alert(`${sum()}`)
-    }
+    const handleClick = () => {
+        alert(`${sum()}`);
+    };
 
     return (
         <div className="flex flex-col items-center justify-center m-10">
             <h1 className="bg-gray-400 m-3 rounded text-white">Home</h1>
-            <button onClick={handleClick} className="bg-green-400 rounded text-white m-3">
+            <button
+                onClick={handleClick}
+                className="bg-green-400 rounded text-white m-3"
+            >
                 Add 2+2
             </button>
-            <button className="bg-green-400 rounded text-white m-3">
+            <button
+                onClick={() => setIsAdmin(!isAdmin)}
+                className="bg-green-400 rounded text-white m-3"
+            >
                 Toggle Admin
             </button>
+            {isAdmin ? <AdminData /> : <h2>Not Admin</h2>}
         </div>
     );
 }
