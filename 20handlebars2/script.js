74ed4fd50ff61a9d42ce7105770e4b486843a437
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-07-22 15:57:02 +0600

    separte handlebars project

--- 20handlebars2/script.js
+++
@@ -1,9 +0,0 @@
-import { cast } from "./data.js";
-
-const div = document.getElementById("show");
-const source = document.getElementById("source").innerHTML;
-const template = Handlebars.compile(source);
-
-const result = template(cast);
-
-div.innerHTML = result;
